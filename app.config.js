5c7ccff0bf97dd1fbaa0ed5233ee6ebb00d8205f
manickamsandeep0-code <you@example.com>
2025-11-29 13:31:25 +0530

    Add Android package name

--- app.config.js
+++ app.config.js
@@ -2,6 +2,9 @@ module.exports = {
   expo: {
     name: 'Karunya Cycle Rental',
     slug: 'cycle-rental-app',
+    android: {
+      package: 'com.karunya.cyclerental'
+    },
     extra: {
       eas: {
         projectId: "c79c3ffa-658e-4d98-9431-13ba061badf8"
