2218b2372a57b9b86c2d8b707c4b9d69d65126c4
manickamsandeep0-code <you@example.com>
2025-11-29 13:30:17 +0530

    Add EAS project ID to config

--- app.config.js
+++ app.config.js
@@ -2,5 +2,10 @@ module.exports = {
   expo: {
     name: 'Karunya Cycle Rental',
     slug: 'cycle-rental-app',
+    extra: {
+      eas: {
+        projectId: "c79c3ffa-658e-4d98-9431-13ba061badf8"
+      }
+    }
   },
 };
